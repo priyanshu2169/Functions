6b3d2f6a8c3932231fe181ee3d16dce5acad1228
priyanshu2169 <priyanshutyagi0910@gmail.com>
2026-02-09 21:41:33 +0530

    check prime

---
+++ primeNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class primeNumber {
+    public static boolean checkPrime(int n){
+        
+        if(n < 2){
+           System.out.println("1 is not a prime or not a composite");
+           return false;
+        }
+        if(n==2){
+            return true;
+        }
+        for(int i=2;i<=(n-1);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n=sc.nextInt();
+        System.out.println(checkPrime(n));
+        
+    }
+}
